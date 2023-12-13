22eb501ecffb94b50fee5d0b776ef55ca79f63fe
Ronabundy1 <s1185083@student.windesheim.nl>
2023-12-13 08:35:48 +0100

    Update interactive.js

---
+++ styles/interactive.js
@@ -0,0 +1,14 @@
+
+
+    probleemButton.addEventListener('click', function() {
+        probleemSection.classList.remove('hidden');
+        oplossingSection.classList.add('hidden');
+    });
+
+    oplossingButton.addEventListener('click', function() {
+        probleemSection.classList.add('hidden');
+        oplossingSection.classList.remove('hidden');
+    });
+
+    
+
