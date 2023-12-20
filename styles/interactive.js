e7d188789db213f14b635f4835ff382e70c77038
Ronabundy1 <s1185083@student.windesheim.nl>
2023-12-20 06:20:11 +0100

    script in aparte .js file

--- styles/interactive.js
+++ styles/interactive.js
@@ -1,14 +1,21 @@
 
 
-    probleemButton.addEventListener('click', function() {
-        probleemSection.classList.remove('hidden');
-        oplossingSection.classList.add('hidden');
-    });
+    document.addEventListener
+        const probleemButton = document.getElementById('probleemButton');
+        const oplossingButton = document.getElementById('oplossingButton');
+        const probleemSection = document.querySelector('.probleem_section');
+        const oplossingSection = document.querySelector('.oplossing_section');
 
-    oplossingButton.addEventListener('click', function() {
-        probleemSection.classList.add('hidden');
-        oplossingSection.classList.remove('hidden');
-    });
+        probleemButton.addEventListener('click', function() {
+            probleemSection.classList.remove('hidden');
+            oplossingSection.classList.add('hidden');
+        });
+
+        oplossingButton.addEventListener('click', function() {
+            probleemSection.classList.add('hidden');
+            oplossingSection.classList.remove('hidden');
+        });
+    ;
 
     
 
